e21845047c4b84d8dbc0a1b13f2b7571d9d40736
yudong <never_yu@foxmail.com>
2020-06-11 23:06:28 +0800

    feat(many file): req headers & resp data modify

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -84,14 +84,8 @@ axios({
     a: 1,
     b: 2
   }
-})
-
-const paramsString = 'q=URLUtils.searchParams&topic=api'
-let searchParams = new URLSearchParams(paramsString)
-axios({
-  method: 'post',
-  url: '/base/post',
-  data: searchParams
+}).then(res => {
+  console.log(res)
 })
 
 const arr = new Int32Array([21, 31])
